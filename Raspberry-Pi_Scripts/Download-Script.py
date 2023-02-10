feb8aa4dd559340784b7bcd96d1f2de7b08e2561
Mahmoud Hussein Goda <mhmoudhgh@gmail.com>
2023-02-10 12:12:29 +0200

    Bug Fixes

--- Raspberry-Pi_Scripts/Download-Script.py
+++ Raspberry-Pi_Scripts/Download-Script.py
@@ -40,12 +40,13 @@ class ThirdTabLoads(QWidget):
         print(f"File has been decrypted successfully and saved to {decrypted_file_path}")
 
 
-    def download_file(self,past_version):    
-        MetaData = 'https://drive.google.com/drive/folders/1puoqf_UirRiVxkcEQswCr_uah8xzKnJ0?usp=share_link'
+    def download_file(self):    
         url = 'https://drive.google.com/drive/folders/1jow0y1TiAY8OkjUaGkmhYM3jWm6RaWYO?usp=share_link'
         current_version = 0
-        gdown.download_folder(MetaData)
-        f = open("MetaData.txt", "r")
+        gdown.download_folder(url)
+        f = open("FOTA-Version-Control/MetaData.txt", "r")
+        f_oldversion = open("FOTA-Version-Control/ITI_STM32F401CC_encryptedv5", "r")
+        past_version
         Lines = f.readlines()
         for items in Lines:
             items = items.split(':')
@@ -71,6 +72,39 @@ class ThirdTabLoads(QWidget):
             msg2.setIcon(QMessageBox.Critical)
             msg2.exec_()
 
+
+    def download_file_GSM(self):    
+        url = 'https://drive.google.com/drive/folders/1KGUhNt6M64gnHKs3s68Umok8oSabzraZ?usp=share_link'
+        current_version = 0
+        #change to download file
+        gdown.download_folder(url)
+        f = open("FOTA-Version-Control/MetaData.txt", "r")
+        #get past version then delete metadata
+        past_version
+        Lines = f.readlines()
+        for items in Lines:
+            items = items.split(':')
+            print(items)
+            if items[0] == "version":
+                current_version = int(items[1])
+            elif items[0] == "filename":
+                name = items[1]
+        print(current_version)
+        if current_version > past_version :
+            gdown.download_folder(url)
+            msg1 = QMessageBox()
+            msg1.setWindowTitle("Uploaded Suceesfully")
+            msg1.setText(f"File {name} has been Downloaded successfully and saved the system")
+            msg1.setIcon(QMessageBox.Information)
+            msg1.exec_()
+            self.decrypt_file()
+
+        else:
+            msg2 = QMessageBox()
+            msg2.setWindowTitle("Error")
+            msg2.setText("There is no New versions available")
+            msg2.setIcon(QMessageBox.Critical)
+            msg2.exec_()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
